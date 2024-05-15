9b517623aca7ec430c9b1f6ba3edcf7c0a17b6d3
karlrobeck <119088381+karlrobeck@users.noreply.github.com>
2024-05-15 22:08:00 +0800

    fix map tiling

--- src/routes/maps/+page.svelte
+++ src/routes/maps/+page.svelte
@@ -8,9 +8,10 @@
 
 	onMount(() => {
 		const initialState = { lng: 121.02904, lat: 14.69766, zoom: 16 };
+		maptilersdk.config.apiKey = API_KEY;
 		map = new maptilersdk.Map({
 			container: 'map',
-			style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+			style: `81f6dc3d-e229-4e80-b504-d0b750984104`,
 			center: [initialState.lng, initialState.lat],
 			zoom: initialState.zoom
 		});
