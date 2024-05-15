041b6a358aa0541a6e6ec20af373a620aad9db42
karlrobeck <119088381+karlrobeck@users.noreply.github.com>
2024-05-15 21:45:35 +0800

    initial commit

---
+++ src/routes/maps/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import * as maptilersdk from '@maptiler/sdk';
+	import { onDestroy, onMount } from 'svelte';
+
+	let map: maptilersdk.Map | undefined = undefined;
+
+	const API_KEY = '17pE2Nv1XmrNauiHohBm';
+
+	onMount(() => {
+		const initialState = { lng: 121.02904, lat: 14.69766, zoom: 16 };
+		map = new maptilersdk.Map({
+			container: 'map',
+			style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+			center: [initialState.lng, initialState.lat],
+			zoom: initialState.zoom
+		});
+	});
+
+	onDestroy(() => {
+		map?.remove();
+	});
+</script>
+
+<div id="map" class="h-screen rounded-md">Hello</div>
